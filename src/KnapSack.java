bc2885f59ce2c6a25962f1e902a870463799488d
SalmaAhmed20 <salma.ahmed.anees@stud.cu.edu.eg>
2021-11-08 22:58:08 +0200

    population + crossover

--- src/KnapSack.java
+++ src/KnapSack.java
@@ -4,15 +4,16 @@ public class KnapSack {
     //problem parameters
     private int numberOfItem;
     private double knapsackSize;
-    ArrayList[][] pairs; //weight and values
+    int[][] pairs; //weight and values
     //GA parameter
     private int chromosomeLength; //= number of items
     private int population_size;
     private ArrayList<String> Population;
+    private ArrayList<String> Crossoverresult;
     private double Pc; //crossover [0.4->0.7] e.g. 0.6
     private double Pm; //mutation [0.001->0.1] e.g. 0.015
 
-    KnapSack(int numberOfItem,double knapsackSize, ArrayList[][] pairs){
+    KnapSack(int numberOfItem, double knapsackSize, int[][] pairs){
         this.numberOfItem =numberOfItem;
         this.chromosomeLength=numberOfItem;
         this.knapsackSize=knapsackSize;
@@ -20,12 +21,48 @@ public class KnapSack {
         //initialize  parameters
         this.Pc=0.6;
         this.Pm=0.015;
-        //this.population_size=50;
+        //
+        this.population_size=numberOfItem;
 
         //call function to initialize population
     }
     private void initializePopulation()
     {
-        String chromosome;
+        //Make pop size chromosome
+        for (int i = 0; i < this.population_size; i++) {
+            String chromosome = "";
+            double RemaingWeight=  this.knapsackSize;
+            //generate gene randomly
+            for (int j = 0; j < this.chromosomeLength; j++) {
+                int gene = (int) Math.round( Math.random() );
+                // to achieve constraint if we pick an item (1)
+                // it should be in weight  capacity
+                if(gene==1)
+                    RemaingWeight=RemaingWeight-pairs[j][0];
+                if (RemaingWeight >= 0||gene==0) {
+                    chromosome += gene;
+                }
+                else {
+                    chromosome+="0".repeat(chromosomeLength-j);
+                }
+            }
+            Population.add(chromosome);
+        }
+    }
+    //Crossover
+    private boolean CrossOver(String chromosome1, String chromosome2){
+        String offString1,offString2;
+        double r2=Math.random();//->[0,1[
+        //we generate r2 before to performance
+        if(r2 <= this.Pc){
+            //Math.floor(Math.random()*(max-min+1)+min)
+            int CrossOverPoint = (int) Math.floor(Math.random()*((this.chromosomeLength-1)-1+1)+1);//->[1,l-1]
+            offString1=chromosome1.substring(0,CrossOverPoint)+chromosome2.substring(CrossOverPoint);
+            offString2=chromosome2.substring(0,CrossOverPoint)+chromosome1.substring(CrossOverPoint);
+            this.Crossoverresult.add(offString1);
+            this.Crossoverresult.add(offString2);
+            return true;
+        }
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         /*
-        * Take Number of items (N)
-        * Take Size of Knapsack (S)
-        * item is a pair of weight and benefit
-        * Length of chromosome (N)
-        * Encoding Binary
-        * initialize population
-        * Constraint to each chromosome <= knapsack size
-        * Maximize benefit (fitness)*/
+         * Take Number of items (N)
+         * Take Size of Knapsack (S)
+         * item is a pair of weight and benefit
+         * Length of chromosome (N)
+         * Encoding Binary
+         * initialize population
+         * Constraint to each chromosome <= knapsack size
+         * Maximize benefit (fitness)*/
+
+//        CrossOver("10100", "01011");
 
     }
+//Test Crossover
+//    static void CrossOver(String chromosome1, String chromosome2) {
+//        String offString1, offString2;
+//        double r2 = Math.random();
+//        if (r2 <= 0.6) {
+//            //Math.floor(Math.random()*(max-min+1)+min)
+//            int CrossOverPoint = (int) Math.floor(Math.random() * ((5 - 1) - 1 + 1) + 1);//->[1,l-1]
+//            System.out.println(CrossOverPoint);
+//            offString1 = chromosome1.substring(0, CrossOverPoint) + chromosome2.substring(CrossOverPoint);
+//            offString2 = chromosome2.substring(0, CrossOverPoint) + chromosome1.substring(CrossOverPoint);
+//            System.out.println(offString1 + "," + offString2);
+//
+//        }
+//
+//    }
 }
